b416c27db13fb15b53d1a8831ba1cda9e88fe7ac
Brad Beltowski <bradbeltowski@gmail.com>
2023-06-13 18:37:31 -0400

    Full CRUD

--- app/controllers/authorCtrl.js
+++ app/controllers/authorCtrl.js
@@ -1,46 +1,102 @@
 const Author = require('../models/Author');
 
-const getAuthors = (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `${req.method} - Author request made` });
+const getAuthors = async (req, res) => {
+  try {
+    const authors = await Author.find();
+    res.status(200).json({
+      data: authors,
+      success: true,
+      message: `${req.method} - Author request made`,
+    });
+  } catch ({ message }) {
+    res.status(500).json({
+      success: false,
+      message,
+    });
+  }
 };
 
-const getAuthorById = (req, res) => {
-  const { id } = req.params;
-  res.status(200).json({
-    id,
-    success: true,
-    message: `${req.method} - Author request made`,
-  });
+const getAuthorById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const author = await Author.findById(id);
+
+    if (!author) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Author not found' });
+    }
+
+    res.status(200).json({
+      data: author,
+      success: true,
+      message: `${req.method} - Author request made`,
+    });
+  } catch ({ message }) {
+    res.status(500).json({
+      success: false,
+      message,
+    });
+  }
 };
 
 const createAuthor = async (req, res) => {
-  const { author } = req.body;
-  const authorData = await Author.create(author);
-  res.status(200).json({
-    data: authorData,
-    success: true,
-    message: `${req.method} - Author request made`,
-  });
+  try {
+    const { author } = req.body;
+    const authorData = await Author.create(author);
+    res.status(200).json({
+      data: authorData,
+      success: true,
+      message: `${req.method} - Author request made`,
+    });
+  } catch ({ message }) {
+    res.status(500).json({
+      success: false,
+      message,
+    });
+  }
 };
 
-const updateAuthor = (req, res) => {
-  const { id } = req.params;
-  res.status(200).json({
-    id,
-    success: true,
-    message: `${req.method} - Author request made`,
-  });
+const updateAuthor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const author = await Author.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!author) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      data: author,
+      success: true,
+      message: `${req.method} - Author request made`,
+    });
+  } catch ({ message }) {
+    res.status(500).json({
+      success: false,
+      message,
+    });
+  }
 };
 
-const deleteAuthor = (req, res) => {
-  const { id } = req.params;
-  res.status(200).json({
-    id,
-    success: true,
-    message: `${req.method} - Author request made`,
-  });
+const deleteAuthor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Author.findByIdAndDelete(id);
+    res.status(200).json({
+      id,
+      success: true,
+      message: `${req.method} - Author request made`,
+    });
+  } catch ({ message }) {
+    res.status(500).json({
+      message,
+      success: false,
+    });
+  }
 };
 
 module.exports = {
