b5e4d2a6c49dc7167f0235cd421738d91799cc18
Brad Beltowski <bradbeltowski@gmail.com>
2024-02-23 15:25:01 -0500

    db

--- app/db/config.js
+++ app/db/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_TEST_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
