b04c09d506f89f6164d9164a02aa87328efd588e
Brad Beltowski <bradbeltowskigmail.com>
2024-03-27 16:23:23 -0400

    Added new items

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import API from "./API";
+import API from "../";
 
 function App() {
   const [formData, setFormData] = useState({});
