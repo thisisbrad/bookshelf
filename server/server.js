bdcba65700b58631831300c920f3f940e9ef15e2
Brad Beltowski <bradbeltowskigmail.com>
2024-05-15 15:01:16 -0400

    connected and added Model

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const app = require("./app");
 require("dotenv").config();
+const connectDB = require("../server/app/db/config");
+
+connectDB();
 
 const PORT = process.env.PORT || 4000;
-console.log(process.env);
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`);
