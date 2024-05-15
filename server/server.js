572a3f6ae2be4229c7c5393b28bffd0a6d00fc6d
Brad Beltowski <bradbeltowskigmail.com>
2024-05-15 14:43:31 -0400

    add

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+console.log(process.env);
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`);
